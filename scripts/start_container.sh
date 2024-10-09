9276e7a6477f519cecc8251bc55fdbd46b7d3981
abhi2k19 <159946105+abhi2k19@users.noreply.github.com>
2024-10-09 19:35:29 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Pull the docker image from dockerhub
-docker pull abhijithas1999/demo_calcu_python
+docker pull abhijithas1999/demo_calcu_python:latest
 
 # Run the docker image as a container
 
-docker run -d -p 8000:8000 abhijithas1999/demo_calcu_python
+docker run -d -p 8000:8000 abhijithas1999/demo_calcu_python:latest
