4e4722a10f0795fbf48073b4e347039ff3af732a
abhi2k19 <159946105+abhi2k19@users.noreply.github.com>
2024-10-10 14:43:08 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,26 @@
 
 set -e
 
-# Pull the docker image from dockerhub
-docker pull abhijithas1999/demo_calcu_python:latest
+# Define variables
+IMAGE_NAME="abhijithas1999/demo_calcu_python:latest"
+CONTAINER_NAME="demo_calcu_container"
+PORT=8000
 
-# Run the docker image as a container
+# Pull the docker image from Docker Hub
+echo "Pulling Docker image..."
+docker pull "$IMAGE_NAME"
 
-docker run -d -p 8000:8000 abhijithas1999/demo_calcu_python:latest
+# Check if the container is already running
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Stopping the running container..."
+    docker stop "$CONTAINER_NAME"
+    echo "Removing the old container..."
+    docker rm "$CONTAINER_NAME"
+fi
+
+# Run the docker image as a new container
+echo "Starting new container..."
+docker run -d --name "$CONTAINER_NAME" -p "$PORT:$PORT" "$IMAGE_NAME"
+
+echo "Container '$CONTAINER_NAME' is now running on port $PORT."
+
